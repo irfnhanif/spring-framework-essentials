adf27bf5c27ccd56c70043bece975c3a28e9a631
irfnhanif <14.irfanh@gmail.com>
2024-08-10 19:23:39 +0700

    Finish module 5's lab in Spring boot Course

--- 38-rest-ws/src/main/java/accounts/web/AccountController.java
+++ 38-rest-ws/src/main/java/accounts/web/AccountController.java
@@ -8,9 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import rewards.internal.account.Account;
 import rewards.internal.account.Beneficiary;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.List;
 
@@ -65,8 +67,9 @@ public class AccountController {
 	 */
 	// TODO-06: Complete this method. Add annotations to:
 	// a. Respond to POST /accounts requests
-    // b. Use a proper annotation for creating an Account object from the request
-	public ResponseEntity<Void> createAccount(Account newAccount) {
+    // b. Use a proper annotation for creating an Account object from the
+	@PostMapping("/accounts")
+	public ResponseEntity<Void> createAccount(@RequestBody Account newAccount) {
 		// Saving the account also sets its entity Id
 		Account account = accountManager.save(newAccount);
 
@@ -90,8 +93,13 @@ public class AccountController {
 		// a. You will need to use 'ServletUriComponentsBuilder' and
 		//     'ResponseEntity' to implement this - Use ResponseEntity.created(..)
 		// b. Refer to the POST example in the slides for more information
+		URI locationURI = ServletUriComponentsBuilder
+				.fromCurrentRequestUri()
+				.path("/{accountId}")
+				.buildAndExpand(resourceId)
+				.toUri();
 
-		return null; // Return something other than null
+		return ResponseEntity.created(locationURI).build(); // Return something other than null
 	}
 
 	/**
@@ -112,14 +120,16 @@ public class AccountController {
 	// a. Respond to a POST /accounts/{accountId}/beneficiaries
 	// b. Extract a beneficiary name from the incoming request
 	// c. Indicate a "201 Created" status
-	public ResponseEntity<Void> addBeneficiary(long accountId, String beneficiaryName) {
+	@PostMapping("/accounts/{accountId}/beneficiaries")
+	public ResponseEntity<Void> addBeneficiary(@PathVariable long accountId, @RequestBody String beneficiaryName) {
 		
 		// TODO-11: Create a ResponseEntity containing the location of the newly
 		// created beneficiary.
 		// a. Use accountManager's addBeneficiary method to add a beneficiary to an account
 		// b. Use the entityWithLocation method - like we did for createAccount().
+		accountManager.addBeneficiary(accountId, beneficiaryName);
 		
-		return null;  // Modify this to return something
+		return entityWithLocation(beneficiaryName);  // Modify this to return something
 	}
 
 	/**
@@ -129,7 +139,9 @@ public class AccountController {
 	// TODO-12: Complete this method by adding the appropriate annotations to:
 	// a. Respond to a DELETE to /accounts/{accountId}/beneficiaries/{beneficiaryName}
 	// b. Indicate a "204 No Content" status
-	public void removeBeneficiary(long accountId, String beneficiaryName) {
+	@DeleteMapping("/accounts/{accountId}/beneficiaries/{beneficiaryName}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void removeBeneficiary(@PathVariable long accountId, @PathVariable String beneficiaryName) {
 		Account account = accountManager.getAccount(accountId);
 		if (account == null) {
 			throw new IllegalArgumentException("No such account with id " + accountId);
